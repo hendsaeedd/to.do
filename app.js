e5ffd4aa40f949d3d9715ef2624ed4f6bfdbec0e
hendsaeedd <hendsaied135@gmail.com>
2024-02-12 20:29:34 +0200

    add swagger

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const cors = require('cors')
 const userRoutes = require('./src/routes/userRoutes')
 const todoRoutes = require('./src/routes/todoRoutes')
 
+//swagger
+const swaggerjsdoc = require('swagger-jsdoc')
+const swaggerui = require('swagger-ui-express')
+
 //middlewares
 //encode url
 app.use(express.urlencoded({ extended: true }))
@@ -33,6 +37,27 @@ mongoose
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//swagger
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Todo API',
+      version: '1.0.1',
+      description: 'A simple Todo API in Express',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+}
+
+const spacs = swaggerjsdoc(options)
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(spacs))
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
