8f359744da22e6683b1cb35b06826471fe0a38d4
hendsaeedd <hendsaied135@gmail.com>
2024-02-12 20:52:14 +0200

    create api documentation using swagger in user's routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,19 +8,135 @@ const {
   updateUser,
 } = require('../controllers/user')
 
-//register user
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User APIs
+ */
+
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               firstname:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User was registered successfully
+ *       '400':
+ *         description: Error registering user
+ */
+
 router.post('/register', registerUser)
 
-//login user
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User was logged in successfully
+ *       '401':
+ *         description: Invalid credentials
+ */
+
 router.post('/login', loginUser)
 
-//get all users firstname
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all users' firstnames
+ *     tags: [Users]
+ *     responses:
+ *       '200':
+ *         description: A list of users' first names
+ *       '400':
+ *         description: Error getting users
+ */
+
 router.get('/', getAllUsers)
 
-//delete user
+/**
+ * @swagger
+ * /users/{name}:
+ *   delete:
+ *     summary: Delete a user by username
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: User deleted successfully
+ *       '400':
+ *         description: Error deleting user
+ */
+
 router.delete('/:name', deleteUser)
 
-//update user
+/**
+ * @swagger
+ * /users/{name}:
+ *   patch:
+ *     summary: Update user information
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               firstname:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: User updated successfully
+ *       '400':
+ *         description: Error updating user
+ */
+
 router.patch('/:name', updateUser)
 
 module.exports = router
